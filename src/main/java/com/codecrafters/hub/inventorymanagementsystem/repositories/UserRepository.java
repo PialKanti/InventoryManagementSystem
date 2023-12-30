4be5b1ec654bbb24a55d308d8f2d814159e9d87c
Pial Kanti Samadder <pialkanti2012@gmail.com>
2023-12-30 20:57:26 +0600

    Enabled EntityGraph in user repository method to avoid org.hibernate.LazyInitializationException

--- src/main/java/com/codecrafters/hub/inventorymanagementsystem/repositories/UserRepository.java
+++ src/main/java/com/codecrafters/hub/inventorymanagementsystem/repositories/UserRepository.java
@@ -1,10 +1,14 @@
 package com.codecrafters.hub.inventorymanagementsystem.repositories;
 
 import com.codecrafters.hub.inventorymanagementsystem.entities.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = "roles")
+    @Transactional(readOnly = true)
     Optional<User> findByUsername(String username);
 }
