7093b3148b75e046d8abce30d2d530242463e996
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-01-01 22:44:46 +0600

    Added @Repository annotation to PermissionRepository class

--- src/main/java/com/codecrafters/hub/inventorymanagementsystem/repositories/PermissionRepository.java
+++ src/main/java/com/codecrafters/hub/inventorymanagementsystem/repositories/PermissionRepository.java
@@ -2,9 +2,11 @@ package com.codecrafters.hub.inventorymanagementsystem.repositories;
 
 import com.codecrafters.hub.inventorymanagementsystem.entities.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
     Optional<Permission> findByKey(String key);
 }
