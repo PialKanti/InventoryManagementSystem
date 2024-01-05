fcd475f93bfa80b72b8c58ce25b49344a4c63a45
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-01-05 13:22:18 +0600

    Implemented User Controller endpoints

---
+++ src/main/java/com/codecrafters/hub/inventorymanagementsystem/controllers/UserController.java
@@ -0,0 +1,87 @@
+package com.codecrafters.hub.inventorymanagementsystem.controllers;
+
+import com.codecrafters.hub.inventorymanagementsystem.entities.User;
+import com.codecrafters.hub.inventorymanagementsystem.entities.request.ChangePasswordRequest;
+import com.codecrafters.hub.inventorymanagementsystem.entities.request.UserUpdateRequest;
+import com.codecrafters.hub.inventorymanagementsystem.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserController(UserRepository repository, PasswordEncoder passwordEncoder) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        var user = repository.findById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody UserUpdateRequest request) {
+        if (!id.equals(request.getId())) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var userOptional = repository.findById(id);
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        User user = userOptional.get();
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+
+        User updatedUser = repository.save(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        var userOptional = repository.findById(id);
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}/password")
+    public ResponseEntity<Map<String, String>> updatePassword(@PathVariable Long id, @RequestBody ChangePasswordRequest request) {
+        var userOptional = repository.findById(id);
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        User user = userOptional.get();
+        if (!passwordEncoder.matches(user.getPassword(), request.getOldPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Collections.singletonMap("message", "Invalid old password"));
+        }
+
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+        repository.save(user);
+        return ResponseEntity.noContent().build();
+    }
+}
