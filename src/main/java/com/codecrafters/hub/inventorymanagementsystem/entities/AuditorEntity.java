0c41da96a88e85a707c928d6470fbf94ec3ba4a2
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-01-06 14:33:03 +0600

    Introduced AuditorEntity base class for Auditing

---
+++ src/main/java/com/codecrafters/hub/inventorymanagementsystem/entities/AuditorEntity.java
@@ -0,0 +1,61 @@
+package com.codecrafters.hub.inventorymanagementsystem.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AuditorEntity {
+    @CreatedBy
+    @Column(name = "created_by")
+    private String createdBy;
+    @CreatedDate
+    @Column(name = "created_on")
+    private LocalDateTime createdOn;
+    @LastModifiedBy
+    @Column(name = "last_modified_by")
+    private String lastModifiedBy;
+    @LastModifiedDate
+    @Column(name = "last_modified_on")
+    private LocalDateTime lastModifiedOn;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedOn() {
+        return lastModifiedOn;
+    }
+
+    public void setLastModifiedOn(LocalDateTime lastModifiedOn) {
+        this.lastModifiedOn = lastModifiedOn;
+    }
+}
