30ad2441bab298d6489bf627e3744fb7e6b29760
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-01-15 22:43:43 +0600

    Added lombok annotation for AuditableEntity and BaseEntity

--- src/main/java/com/codecrafters/hub/inventorymanagementsystem/entities/common/AuditableEntity.java
+++ src/main/java/com/codecrafters/hub/inventorymanagementsystem/entities/common/AuditableEntity.java
@@ -3,6 +3,8 @@ package com.codecrafters.hub.inventorymanagementsystem.entities.common;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -13,6 +15,8 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
 public abstract class AuditableEntity extends BaseEntity {
     @CreatedBy
     @Column(name = "created_by")
@@ -26,36 +30,4 @@ public abstract class AuditableEntity extends BaseEntity {
     @LastModifiedDate
     @Column(name = "last_modified_on")
     private LocalDateTime lastModifiedOn;
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public LocalDateTime getCreatedOn() {
-        return createdOn;
-    }
-
-    public void setCreatedOn(LocalDateTime createdOn) {
-        this.createdOn = createdOn;
-    }
-
-    public String getLastModifiedBy() {
-        return lastModifiedBy;
-    }
-
-    public void setLastModifiedBy(String lastModifiedBy) {
-        this.lastModifiedBy = lastModifiedBy;
-    }
-
-    public LocalDateTime getLastModifiedOn() {
-        return lastModifiedOn;
-    }
-
-    public void setLastModifiedOn(LocalDateTime lastModifiedOn) {
-        this.lastModifiedOn = lastModifiedOn;
-    }
 }
--- src/main/java/com/codecrafters/hub/inventorymanagementsystem/entities/common/BaseEntity.java
+++ src/main/java/com/codecrafters/hub/inventorymanagementsystem/entities/common/BaseEntity.java
@@ -4,18 +4,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 
 @MappedSuperclass
+@Getter
+@Setter
 abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
