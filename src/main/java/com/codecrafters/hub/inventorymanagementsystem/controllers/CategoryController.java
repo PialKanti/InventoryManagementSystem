99a399f439c965da53874a019da853e270f501f1
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-01-19 13:27:01 +0600

    Made category find all endpoint to return pageable response

--- src/main/java/com/codecrafters/hub/inventorymanagementsystem/controllers/CategoryController.java
+++ src/main/java/com/codecrafters/hub/inventorymanagementsystem/controllers/CategoryController.java
@@ -1,17 +1,19 @@
 package com.codecrafters.hub.inventorymanagementsystem.controllers;
 
 import com.codecrafters.hub.inventorymanagementsystem.dtos.request.categories.CategoryCreateRequest;
+import com.codecrafters.hub.inventorymanagementsystem.dtos.response.BasePaginatedResponse;
 import com.codecrafters.hub.inventorymanagementsystem.dtos.response.EntityResponse;
 import com.codecrafters.hub.inventorymanagementsystem.entities.projections.CategoryProjection;
 import com.codecrafters.hub.inventorymanagementsystem.services.CategoryService;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/v1/categories")
@@ -24,8 +26,10 @@ public class CategoryController {
     }
 
     @GetMapping
-    public ResponseEntity<List<CategoryProjection>> findAll() {
-        return ResponseEntity.ok(service.findAll(CategoryProjection.class));
+    public ResponseEntity<BasePaginatedResponse<CategoryProjection>> findAll(@RequestParam(name = "page", defaultValue = "0", required = false) int page,
+                                                                             @RequestParam(name = "pageSize", defaultValue = "5", required = false) int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+        return ResponseEntity.ok(service.findAll(pageable, CategoryProjection.class));
     }
 
     @GetMapping(value = "/{id}")
