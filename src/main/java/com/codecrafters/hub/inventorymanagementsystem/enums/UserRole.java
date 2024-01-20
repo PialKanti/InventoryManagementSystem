cab4078230eea2f7f3833c6d20fa012a8294041d
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-01-20 16:28:45 +0600

    Used lombok @Getter in UserRole enum

--- src/main/java/com/codecrafters/hub/inventorymanagementsystem/enums/UserRole.java
+++ src/main/java/com/codecrafters/hub/inventorymanagementsystem/enums/UserRole.java
@@ -1,14 +1,13 @@
 package com.codecrafters.hub.inventorymanagementsystem.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum UserRole {
     ADMIN("Admin"),
     USER("User");
 
     private final String displayValue;
-
-    public String getDisplayValue() {
-        return displayValue;
-    }
 
     UserRole(String displayValue) {
         this.displayValue = displayValue;
