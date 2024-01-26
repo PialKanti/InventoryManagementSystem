c0518568a21969759ca0c89809254eb9f053f5fe
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-01-26 22:01:27 +0600

    Added BlackListedTokenRepository unit test class

---
+++ src/test/java/com/codecrafters/hub/inventorymanagementsystem/repositories/tokens/BlackListedTokenRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.codecrafters.hub.inventorymanagementsystem.repositories.tokens;
+
+import org.junit.jupiter.api.Test;
+
+class BlackListedTokenRepositoryTest {
+
+    @Test
+    void findByToken() {
+    }
+}
