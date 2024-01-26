f5d12c8b5f970050027cbc9ecc5c62c3b41c93e0
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-01-26 19:51:55 +0600

    Removed unused imports

--- src/main/java/com/codecrafters/hub/inventorymanagementsystem/controllers/CategoryController.java
+++ src/main/java/com/codecrafters/hub/inventorymanagementsystem/controllers/CategoryController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/v1/categories")
