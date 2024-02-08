93437b20719aec3da50286d1b65a9dc0d909de16
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-02-08 17:58:20 +0600

    Added login page design

---
+++ ClientApp/online-shopping-app/src/components/LoginForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card title="Login">
+        <v-form @submit.prevent class="login-form">
+            <v-text-field v-model="email" label="Email" variant="outlined"></v-text-field>
+            <v-text-field v-model="password" label="Password" type="password" variant="outlined"></v-text-field>
+            <v-btn type="submit" class="mt-2" variant="outlined">Submit</v-btn>
+        </v-form>
+    </v-card>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.login-form {
+    padding-top: 30px;
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-bottom: 20px;
+}
+</style>
---
+++ ClientApp/online-shopping-app/src/pages/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container class="d-flex justify-center align-center fill-height">
+        <LoginForm class="loginForm" />
+    </v-container>
+</template>
+  
+<script setup>
+//
+</script>
+
+<style scoped>
+.loginForm {
+    width: 30vw;
+}
+</style>
+  
