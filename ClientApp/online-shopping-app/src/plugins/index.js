a7fe591c8e3c1d21425f026956197a33422703da
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-02-09 11:30:34 +0600

    Added pinia for store management

--- ClientApp/online-shopping-app/src/plugins/index.js
+++ ClientApp/online-shopping-app/src/plugins/index.js
@@ -5,11 +5,13 @@
  */
 
 // Plugins
+import pinia from '@/stores'
 import vuetify from './vuetify'
 import router from '@/router'
 
 export function registerPlugins (app) {
   app
     .use(vuetify)
+    .use(pinia)
     .use(router)
 }
