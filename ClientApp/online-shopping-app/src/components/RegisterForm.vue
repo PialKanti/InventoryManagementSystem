4706a4668bb0a36e93015ff175e88fb231e1e450
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-02-09 22:57:07 +0600

    Added registration functionality for client

--- ClientApp/online-shopping-app/src/components/RegisterForm.vue
+++ ClientApp/online-shopping-app/src/components/RegisterForm.vue
@@ -1,6 +1,6 @@
 <template>
     <v-card title="Register">
-        <v-form @submit.prevent class="register-form">
+        <v-form @submit.prevent="register" class="register-form">
             <v-alert class="register-alert" v-if="isAlertShown" :text="alertMessage" :type="alertType"></v-alert>
             <v-row>
                 <v-col>
@@ -20,7 +20,9 @@
 </template>
 
 <script setup>
+import axios, { HttpStatusCode } from 'axios';
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 const firstname = ref('');
 const lastname = ref('');
@@ -28,6 +30,40 @@ const username = ref('');
 const email = ref('');
 const password = ref('');
 const confirmPassword = ref('');
+
+const router = useRouter();
+
+const register = async () => {
+    const data = {
+        firstName: firstname.value,
+        lastName: lastname.value,
+        username: username.value,
+        email: email.value,
+        password: password.value
+    };
+
+    console.log(data);
+
+    await axios.post('/api/auth/register', data)
+        .then(response => {
+            console.log(response);
+
+            if (response.status === HttpStatusCode.Ok) {
+                router.push({ path: '/login' });
+            }
+        })
+        .catch(error => {
+            console.error('Error occured.', error);
+            showErrorAlert('Something went wrong. Please try again.');
+        });
+
+};
+
+const showErrorAlert = (message) => {
+    isAlertShown.value = true;
+    alertType.value = 'error';
+    alertMessage.value = message;
+};
 </script>
 
 <style scoped>
