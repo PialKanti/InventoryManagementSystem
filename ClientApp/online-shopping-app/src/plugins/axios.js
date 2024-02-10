438b47a09ec540f8f4897eb209744153e80abfae
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-02-10 23:37:27 +0600

    Added Authorization header for axios globally except login and register

--- ClientApp/online-shopping-app/src/plugins/axios.js
+++ ClientApp/online-shopping-app/src/plugins/axios.js
@@ -1,7 +1,19 @@
+import { useAuthStore } from "@/stores/authStore";
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL
+    baseURL: import.meta.env.VITE_BASE_URL,
+});
+
+axios.interceptors.request.use((config) => {
+    const authStore = useAuthStore();
+    if (!config.url.includes('/login') && config.url.includes('/register')) {
+        config.headers = {
+            Authorization: 'Bearer ' + authStore.token
+        };
+    }
+
+    return config;
 });
 
 export default axiosInstance;
