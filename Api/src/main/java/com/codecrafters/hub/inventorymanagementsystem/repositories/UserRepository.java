79cb32d287c681a31635042f70864e866093024a
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-02-10 09:35:13 +0600

    Added find by username repository method for getting user projection

--- Api/src/main/java/com/codecrafters/hub/inventorymanagementsystem/repositories/UserRepository.java
+++ Api/src/main/java/com/codecrafters/hub/inventorymanagementsystem/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.codecrafters.hub.inventorymanagementsystem.repositories;
 
 import com.codecrafters.hub.inventorymanagementsystem.entities.User;
+import com.codecrafters.hub.inventorymanagementsystem.entities.projections.UserProjection;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,4 +13,5 @@ public interface UserRepository extends BaseRepository<User, Long> {
     @EntityGraph(value = "User.roles", type = EntityGraph.EntityGraphType.FETCH)
     @Transactional(readOnly = true)
     Optional<User> findByUsername(String username);
+    Optional<UserProjection> findByUsername(String username, Class<UserProjection> type);
 }
