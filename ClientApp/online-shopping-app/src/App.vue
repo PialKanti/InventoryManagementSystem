eecccf2a9fc10cc6e98109fce5a53484b352ccdc
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-02-12 17:56:56 +0600

    Added navigation drawer to the left side

--- ClientApp/online-shopping-app/src/App.vue
+++ ClientApp/online-shopping-app/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <AppBar v-if="showAppbar" />
+    <NavigationDrawer v-if="showNavigationDrawer" />
     <v-main>
       <router-view />
     </v-main>
@@ -12,9 +13,11 @@ import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 
 const showAppbar = ref(false);
+const showNavigationDrawer = ref(false);
 
 const route = useRoute();
 watch(() => route.path, (path) => {
   showAppbar.value = path !== '/login' && path !== '/register';
+  showNavigationDrawer.value = path !== '/login' && path !== '/register';
 });
 </script>
---
+++ ClientApp/online-shopping-app/src/components/NavigationDrawer.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-navigation-drawer>
+        <v-list>
+            <v-list-item prepend-icon="mdi-package-variant" title="Products" value="products"></v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script setup></script>
