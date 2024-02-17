c77696058a4426d18dfa9d1a3f80d522e5d8bb6f
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-02-17 23:26:43 +0600

    Added to link to navigation drawer item

--- ClientApp/online-shopping-app/src/components/NavigationDrawer.vue
+++ ClientApp/online-shopping-app/src/components/NavigationDrawer.vue
@@ -1,17 +1,10 @@
 <template>
     <v-navigation-drawer>
         <v-list>
-            <v-list-item prepend-icon="mdi-package-variant" title="Products" value="products"
-                @click="clickNavItems"></v-list-item>
+            <v-list-item prepend-icon="mdi-package-variant" title="Products" value="products" to="/products"></v-list-item>
         </v-list>
     </v-navigation-drawer>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-const clickNavItems = () => {
-    router.push({ path: '/products' });
-};
 </script>
