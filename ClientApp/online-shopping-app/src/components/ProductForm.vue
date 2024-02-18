31cc64ebc68f5c996b849b375f6e86450bf454b0
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-02-18 16:05:44 +0600

    Added product create page

---
+++ ClientApp/online-shopping-app/src/components/ProductForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container>
+        <h3>Add Product</h3>
+        <v-row class="mt-4">
+            <v-col cols="6">
+                <v-form>
+                    <v-text-field clearable label="Title" variant="outlined"></v-text-field>
+                    <v-textarea label="Description" variant="outlined"></v-textarea>
+                    <v-row>
+                        <v-col>
+                            <v-text-field label="Price" prefix="৳" variant="outlined"></v-text-field>
+                        </v-col>
+                        <v-col>
+                            <v-text-field label="Amount" type="number" variant="outlined"></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-btn type="submit" class="mt-2 submit-button" color="primary">Create</v-btn>
+                </v-form>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup></script>
