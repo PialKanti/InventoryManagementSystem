b376b4fa0349e7b8fad8e7dce98697c4c51a6abc
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-02-20 17:55:47 +0600

    Added product update page

---
+++ ClientApp/online-shopping-app/src/pages/products/update/[id].vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script setup>
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+onMounted(() => {
+    console.log('Id = ', route.params.id);
+});
+</script>
