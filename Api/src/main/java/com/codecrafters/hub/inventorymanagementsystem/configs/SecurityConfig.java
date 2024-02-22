e0ff98f7187a4e1096a6102ac4196cc3d3e779f5
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-02-22 14:30:16 +0600

    Removed unused imports

--- Api/src/main/java/com/codecrafters/hub/inventorymanagementsystem/configs/SecurityConfig.java
+++ Api/src/main/java/com/codecrafters/hub/inventorymanagementsystem/configs/SecurityConfig.java
@@ -2,10 +2,8 @@ package com.codecrafters.hub.inventorymanagementsystem.configs;
 
 import com.codecrafters.hub.inventorymanagementsystem.filters.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
