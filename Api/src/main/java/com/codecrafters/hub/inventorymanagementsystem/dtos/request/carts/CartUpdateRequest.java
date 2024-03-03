fb9c3e729638bf495d19a20f3b2b1437b4b1d3d3
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-03-03 12:32:03 +0600

    Updated CartUpdateRequest class properties

--- Api/src/main/java/com/codecrafters/hub/inventorymanagementsystem/dtos/request/carts/CartUpdateRequest.java
+++ Api/src/main/java/com/codecrafters/hub/inventorymanagementsystem/dtos/request/carts/CartUpdateRequest.java
@@ -2,6 +2,9 @@ package com.codecrafters.hub.inventorymanagementsystem.dtos.request.carts;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class CartUpdateRequest {
+    private List<CartItemDto> cartItems;
 }
