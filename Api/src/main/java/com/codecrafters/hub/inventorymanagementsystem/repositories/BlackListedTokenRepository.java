6530785157424b34c7a38e2339e2c185668fbd8e
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-07-02 12:08:08 +0600

    Added missing @Repository annotation in BlackListedTokenRepository

--- Api/src/main/java/com/codecrafters/hub/inventorymanagementsystem/repositories/BlackListedTokenRepository.java
+++ Api/src/main/java/com/codecrafters/hub/inventorymanagementsystem/repositories/BlackListedTokenRepository.java
@@ -2,9 +2,11 @@ package com.codecrafters.hub.inventorymanagementsystem.repositories;
 
 import com.codecrafters.hub.inventorymanagementsystem.entities.BlackListedToken;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface BlackListedTokenRepository extends CrudRepository<BlackListedToken, Long> {
     Optional<BlackListedToken> findByToken(String token);
 }
