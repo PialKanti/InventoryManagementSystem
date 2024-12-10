c14f05ddf1902dcf800593e0dd59d9c3874ba422
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-12-10 10:55:59 +0600

    Added java documentation for String null or empty method check

--- Api/src/main/java/com/codecrafters/hub/inventorymanagementsystem/util/StringUtils.java
+++ Api/src/main/java/com/codecrafters/hub/inventorymanagementsystem/util/StringUtils.java
@@ -4,6 +4,13 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor( access = lombok.AccessLevel.PRIVATE)
 public class StringUtils {
+    /**
+     * Checks if a given string is either {@code null} or empty.
+     *
+     * @param input the string to check
+     * @return {@code true} if the string is {@code null} or its length is 0,
+     *         {@code false} otherwise
+     */
     public static boolean isNullOrEmpty(String input){
         return input == null || input.isEmpty();
     }
