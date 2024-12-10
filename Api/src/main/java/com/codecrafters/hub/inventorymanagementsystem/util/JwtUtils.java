e7029d427ad088d53b5eceb5089dd80ef4d3c59e
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-12-10 10:53:41 +0600

    Added private constructor in Util classes

--- Api/src/main/java/com/codecrafters/hub/inventorymanagementsystem/util/JwtUtils.java
+++ Api/src/main/java/com/codecrafters/hub/inventorymanagementsystem/util/JwtUtils.java
@@ -1,5 +1,8 @@
 package com.codecrafters.hub.inventorymanagementsystem.util;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor( access = lombok.AccessLevel.PRIVATE)
 public class JwtUtils {
     public static String extractTokenFromAuthorizationHeader(String authorizationHeader) {
         if (authorizationHeader == null || authorizationHeader.isEmpty()) {
--- Api/src/main/java/com/codecrafters/hub/inventorymanagementsystem/util/StringUtils.java
+++ Api/src/main/java/com/codecrafters/hub/inventorymanagementsystem/util/StringUtils.java
@@ -1,5 +1,8 @@
 package com.codecrafters.hub.inventorymanagementsystem.util;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor( access = lombok.AccessLevel.PRIVATE)
 public class StringUtils {
     public static boolean isNullOrEmpty(String input){
         return input == null || input.isEmpty();
